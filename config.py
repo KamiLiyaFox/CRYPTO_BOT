56c45c26b11c497555338e6620dc9c11fdea1024
Svetlana <122841597+KamiLiyaFox@users.noreply.github.com>
2023-03-05 01:13:58 +0300

    Commit Bot
    05.03.23

---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = "6079636637:AAEGBl7KtLoDPqafr-hnI79ydvvizBp1bnw"
+exchanges = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB'
+}
---
+++ extensions.py
@@ -0,0 +1,35 @@
+import requests
+import json
+from config import exchanges
+
+class APIException(Exception):
+    pass
+
+
+class Converter:
+    @staticmethod
+    def get_price(base: str, quote: str, amount: str):
+
+        if base == quote:
+            raise APIException(f'Невозможно перевести одинаковые валюты {quote}')
+
+        try:
+            base_ticker = exchanges[base]
+        except KeyError:
+            raise APIException(f'Валюта {base} не найдена!')
+
+        try:
+            quote_ticker = exchanges[quote]
+        except KeyError:
+            raise APIException(f'Валюта {quote} не найдена!')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Неудалось обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={base_ticker}&tsyms={quote_ticker}')
+        total_quote = json.loads(r.content)[exchanges[quote]] * float(amount)
+
+        return total_quote
+
